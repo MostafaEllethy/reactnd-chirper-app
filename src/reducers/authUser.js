3d01a7a9455a725d44590dd1dd3dc5a77bf90971
Mostafa Ellethy <mostafa.ellethy93@gmail.com>
2021-11-28 21:24:55 +0200

    ;

--- src/reducers/authUser.js
+++ src/reducers/authUser.js
@@ -3,7 +3,7 @@ import { SET_AUTH_USER } from '../actions/authUser'
 const authUser = (state = null, action) => {
     switch (action.type) {
         case SET_AUTH_USER:
-            return { ...state, ...action.id }
+            return action.id
         default:
             return state;
     }
--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -3,7 +3,7 @@ import { RECEIVE_TWEETS } from '../actions/tweets'
 const tweets = (state = {}, action) => {
     switch (action.type) {
         case RECEIVE_TWEETS:
-            return { ...state, ...action.tweets }
+            return action.tweets
         default:
             return state;
     }
